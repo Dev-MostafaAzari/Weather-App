b14073c84090fb459c6da1ed83071e623791d216
Mostafa <msari808755@gmail.com>
2025-12-27 01:24:16 -0800

    folderStructur

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {motion} from "framer-motion";
+import "../../styles/Main.css";
+import Cloud from "../../assets/Cloud/Cloud1.png";
+
+const Main = () =>{
+    return(
+        <div className="Main">
+            <span>main</span>
+        </div>
+    )
+    
+}
+
+export default Main;

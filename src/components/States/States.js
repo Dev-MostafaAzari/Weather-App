4e585f713729c2b064b20affccb13194f6ca15a1
Mostafa <msari808755@gmail.com>
2026-01-01 00:44:49 -0800

    StatsBasicStyle

--- src/components/States/States.js
+++ src/components/States/States.js
@@ -4,9 +4,28 @@ import "../../styles/States.css";
 
 const States=()=>{
     return(
-        <div className="StatesMain">
-            <div className="StateWrapper">
-                states
+        <div className="StatsMain">
+            <div className="StatsWrapper">
+                <div className="SearchContainer">
+                    <button className="SearchBtn">Search</button>
+                </div>
+                <div className="StatsContainer">
+                    <div className="StatsHeader">
+                        <h1 className="StatsTitle">Title Here</h1>
+                    </div>
+                    <hr className="hr"/>
+                    <div className="StatsData">
+                        <ul className="WeatherData">
+                            <li>Humidity:</li>
+                            <li>Wind Speed:</li>
+                            <li>Cloudiness:</li>
+                            <li>Feels Like:</li>
+                            <li>Pressure:</li>
+                            <li>Visibility:</li>
+                            <li>UV Index:</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/features/weather/weatherSlice.js
+++ src/features/weather/weatherSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    setLocation : false,
+    setLocation : true,
     loading : false,
     weather:{},
 };

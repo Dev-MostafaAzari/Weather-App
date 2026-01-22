8dda81da8347a462e365c024fbe48c9254967b9c
Mostafa <msari808755@gmail.com>
2026-01-22 06:30:01 -0800

    ViewBgAnimation

---
+++ src/components/View/ViewBgAnimation/ViewBgAnimation.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+
+
+
+const BgAnimation = ()=>{
+    useEffect(()=>{
+       
+
+
+
+
+    },[])
+
+
+
+
+
+    return(
+        <div id="BgAnim" className="BgContainer">
+            <canvas id="Canvas">
+
+            </canvas>
+        </div>
+    )
+};
+
+export default BgAnimation;

c73776742545be5c1a7f5ff60670cc4230e8d94a
Mostafa <msari808755@gmail.com>
2026-02-03 09:26:45 -0800

    bgAnimationCheckAndChange

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -22,6 +22,18 @@ const ViewTitleVariants={
 const View = ()=>{
     const data = useSelector(state=>state.weather);
     const loadingData = useSelector(state=>state.loading);
+    const WeatherCheck = (data)=>{
+        if(200<data && data<532)    //Rain
+        {
+            return <RainAnimation/>;
+        }else if(599<data && data<623)   //Snow
+        {
+            return <SnowAnimation/>;
+        }else 
+        {
+            return "null";
+        }
+    };
 
     return(
         <div className="ViewMain">
@@ -37,8 +49,7 @@ const View = ()=>{
                     <>
                         <div className="ViewContainer">
                             <div className="WeatherEffectContainer">
-                                {/* <RainAnimation/> */}
-                                <SnowAnimation/>
+                                {WeatherCheck(data.weather.data.weather[0].id)}    {/* WeatherCheck */}
                                 <div className="ViewTitleContainer">
                                     <motion.div variants={ViewTitleVariants} className="ViewTitleInnerContainer" initial="initial" whileInView="animate">
                                         <motion.h1 variants={ViewTitleVariants} className="ViewTitle">Temperature:<motion.span variants={ViewTitleVariants}>{data.weather.data.main.temp}℃</motion.span></motion.h1>

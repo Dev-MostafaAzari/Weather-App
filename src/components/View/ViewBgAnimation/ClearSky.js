a9f50839378635d9c396dd647146003979838732
Mostafa <msari808755@gmail.com>
2026-02-05 08:27:30 -0800

    ClearSkyXAndYChange

--- src/components/View/ViewBgAnimation/ClearSky.js
+++ src/components/View/ViewBgAnimation/ClearSky.js
@@ -9,13 +9,13 @@ const ClearSky = ()=>{
         <div className="ClearSkyContainer">
             <svg id="ClearSkySvg">
                 <circle cx={"51%"} cy={"20%"} r={100} stroke="Yellow" fill="Yellow" />
-                <motion.g id="Cloud1" fill="#ffffff" initial={{x:600,y:300,scale:2}} animate={{x:550,transition:{duration:20,ease:"linear",repeat:Infinity,repeatType:'reverse'}}} >
+                <motion.g id="Cloud1" fill="#ffffff" initial={{x:"40vw",y:"25vh",scale:2}} animate={{x:"35vw",transition:{duration:20,ease:"linear",repeat:Infinity,repeatType:'reverse'}}} >
                     <motion.circle cx={"70"} cy={"70"} r={"20"}/>
                     <motion.circle cx={"90"} cy={"60"} r={"25"}/>
                     <motion.circle cx={"115"} cy={"70"} r={"20"}/>
                     <motion.rect x={"70"} y={"70"} width={"45"} height={"20"}/>
                 </motion.g>
-                <motion.g id="Cloud2" fill="#ffffff" initial={{x:900,y:100,scale:2}} animate={{x:950,transition:{duration:20,ease:"linear",repeat:Infinity,repeatType:'reverse'}}} >
+                <motion.g id="Cloud2" fill="#ffffff" initial={{x:"25vw",y:100,scale:2}} animate={{x:"30vw",transition:{duration:20,ease:"linear",repeat:Infinity,repeatType:'reverse'}}} >
                     <motion.circle cx={"70"} cy={"70"} r={"20"}/>
                     <motion.circle cx={"90"} cy={"60"} r={"25"}/>
                     <motion.circle cx={"115"} cy={"70"} r={"20"}/>

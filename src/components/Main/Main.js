b277fc011233e2cb8a3ffd5763f644a0355619ce
Mostafa <msari808755@gmail.com>
2026-02-11 23:49:08 -0800

    CleaningCodeBase

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import {motion} from "framer-motion";
 import "../../styles/Main.css";
-import Cloud from "../../assets/Cloud/Cloud1.png";
 import States from '../States/States';
 import View from '../View/View';
 import { useDispatch, useSelector } from 'react-redux';
--- src/components/States/States.js
+++ src/components/States/States.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {motion} from "framer-motion";
 import "../../styles/States.css";
 import { useDispatch, useSelector } from 'react-redux';

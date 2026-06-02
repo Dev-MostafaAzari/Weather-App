6f26041642ed5fc828ed6d33fc921f30fa11d124
Mostafa <msari808755@gmail.com>
2026-06-02 08:28:22 -0700

    LoadingStyleFixAndLoadingStateFixed

--- src/features/weather/weatherSlice.js
+++ src/features/weather/weatherSlice.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const initialState = {
     setLocation : true,
-    loading : true,
+    loading : false,
     weather:{},
     reject:false,
     rejectValue:"",
@@ -50,6 +50,7 @@ const WeatherSlice = createSlice({
         builder.addCase(GetWeather.rejected,(state,action)=>{
             state.setLocation = true;
             state.reject = true;
+            state.loading = false;
             state.rejectValue = action.error.message;
         })
     }

a2dcf18b9226eb6b37ecfc3f490d33aaf216c015
Mostafa <msari808755@gmail.com>
2026-06-02 03:11:47 -0700

    LoadingComponent

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import "../../styles/Loading.css";
+
+const Loading = () => {
+    return (
+        <div className="LoadingContainer">
+            <div className="Circle Cricle-1"></div>
+            <div className="Circle Cricle-2"></div>
+        </div>
+    );
+}
+ 
+export default Loading;
